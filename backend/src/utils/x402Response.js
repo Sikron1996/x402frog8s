519826ebf0eb6698c681c403529f3415b6f8054b
Sikron1996 <vadimmonarh@gmail.com>
2025-11-05 00:50:57 +0200

    Update x402Response.js

--- backend/src/utils/x402Response.js
+++ backend/src/utils/x402Response.js
@@ -9,13 +9,15 @@ export function buildX402Response() {
       {
         scheme: "exact",
         network: "base",
+        chainId: 8453,
         maxAmountRequired: "3000000",
         resource: "https://x402frog8s-one.vercel.app/api/mint",
-        description: "Mint 1 x402frog8s Punk",
+        description: "Mint 1 x402Cats NFT (Gasless via PayAI)",
         mimeType: "application/json",
-        payTo: receiver,
+        payTo: "0xF97a410f2f0b64Cb5820baD63d878c3A967235AA",
         maxTimeoutSeconds: 300,
-        asset: "USDC"
+        asset: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+        facilitator: "https://facilitator.payai.network",
       },
       {
         scheme: "exact",
